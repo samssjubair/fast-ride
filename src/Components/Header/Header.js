66674c32e6645092ca013fa09b81f378ef4af7b1
samssjubair <samssjubair@gmail.com>
2021-03-21 12:53:41 +0600

    all features added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
   return (
     <div>
       <ul>
-        <h1>Fast-Ride</h1>
+        <h2>Fast-Ride</h2>
         <li  className="ms-auto"> <Link style={linkStyle} to="/home">Home</Link> </li>
         <li> <Link style={linkStyle}  to="/ride/bike">Destination</Link></li>
         <li> <Link style={linkStyle}  to="/blog">Blog</Link></li>
